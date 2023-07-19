330d94d6995e89be64e8ad00dc8db7fea61e008d
mr-dargi <dargimohammadreza@gmail.com>
2023-07-19 18:10:53 +0330

    Cleaning up some extra files, create Nav.js component and add it inside App.js

---
+++ src/component/Nav.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+
+export default function Nav() {
+  return (
+    <div className="nav">
+      <img src="./images/troll-face.png" className="nav-img" />
+      <h4 className="nav-title">Meme Generator</h4>
+      <p className="nav-description">React Course - Project 3</p>
+    </div>
+  )
+}
