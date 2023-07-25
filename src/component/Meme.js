173d0a006e6257f268feca7ea2a0c2097a8d531f
mr-dargi <dargimohammadreza@gmail.com>
2023-07-25 17:33:19 +0330

    Change state to object for recive and saving our inputs data inside of itself

--- src/component/Meme.js
+++ src/component/Meme.js
@@ -4,25 +4,33 @@ import memesData from "../memesData";
 
 export default function Meme() {
 
-  const [memeImg, setMemeImg] = React.useState() 
+  const [meme, setMeme] = React.useState({
+    topText: "",
+    bottomText: "",
+    randomImage: "http://i.imgflip.com/1bij.jpg"
+  }) 
   
   
   const generate_random_meme = () => {
     
     const data = memesData.data.memes; 
     const random = Math.floor(Math.random() * data.length)
-    
-    setMemeImg(data[random].url);
+    const url = data[random].url;
+
+    setMeme(prevState => ({
+      ...prevState,
+      randomImage: url
+    }))
   }
 
   return (
     <div className="meme">
       <div className="meme-inputs">
-        <input type="text" />
-        <input type="text" />
+        <input type="text" placeholder="Top text" />
+        <input type="text" placeholder="Bottom text" />
       </div>
       <button className="meme-button" onClick={generate_random_meme}>Get a new meme image  🖼</button>
-      {memeImg && <img src={memeImg} className="meme-img" />}
+      {meme.randomImage && <img src={meme.randomImage} className="meme-img" />}
     </div>
   )
 }
