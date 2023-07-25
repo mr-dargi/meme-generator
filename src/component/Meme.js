c3b449755fa259283b566b153db14aaef1783277
mr-dargi <dargimohammadreza@gmail.com>
2023-07-25 17:50:21 +0330

    Recive data from the inputs and change state with them

--- src/component/Meme.js
+++ src/component/Meme.js
@@ -8,26 +8,46 @@ export default function Meme() {
     topText: "",
     bottomText: "",
     randomImage: "http://i.imgflip.com/1bij.jpg"
-  }) 
+  });
   
   
   const generate_random_meme = () => {
     
     const data = memesData.data.memes; 
-    const random = Math.floor(Math.random() * data.length)
+    const random = Math.floor(Math.random() * data.length);
     const url = data[random].url;
 
     setMeme(prevState => ({
       ...prevState,
       randomImage: url
     }))
-  }
+  };
+
+  const handleChanges = events => {
+    const {name, value} = events.target;
+    setMeme(prevState => ({
+      ...prevState,
+      [name]: value
+    }))
+  };
 
   return (
     <div className="meme">
       <div className="meme-inputs">
-        <input type="text" placeholder="Top text" />
-        <input type="text" placeholder="Bottom text" />
+        <input 
+          type="text" 
+          placeholder="Top text" 
+          name="topText"
+          value={meme.topText}
+          onChange={handleChanges}
+        />
+        <input 
+          type="text" 
+          placeholder="Bottom text" 
+          name="bottomText"
+          value={meme.bottomText}
+          onChange={handleChanges}
+        />
       </div>
       <button className="meme-button" onClick={generate_random_meme}>Get a new meme image  🖼</button>
       {meme.randomImage && <img src={meme.randomImage} className="meme-img" />}
